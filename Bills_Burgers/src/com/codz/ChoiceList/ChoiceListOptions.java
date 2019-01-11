3fc3ea96f1b99858e3303cb7273b7f76589f1cd4
BILL YENGO <yengobilly4063@gmail.com>
2019-01-11 10:50:41 +0200

    Refraction and Reconstruction of Initial Idea

--- Bills_Burgers/src/com/codz/ChoiceList/ChoiceListOptions.java
+++ Bills_Burgers/src/com/codz/ChoiceList/ChoiceListOptions.java
@@ -4,12 +4,29 @@ import java.util.ArrayList;
 
 public class ChoiceListOptions {
 
+    public ChoiceListOptions() {
+        //Needs not variables to be instantiated
+        //By default
+    }
+
+    public static ArrayList<Integer> validBurgerChoices(){
+        ArrayList <Integer> choices = new ArrayList<>();
+        choices.add(1);     //Hamburger
+        choices.add(2);     //HealthyBurger
+        choices.add(3);     //Deluxe Burger
+        return choices;
+    }
+
+
+
     public static ArrayList<Integer> validRegularToppingsChoices(){
         ArrayList <Integer> choices = new ArrayList<>();
-        choices.add(1);
-        choices.add(2);
-        choices.add(3);
-        choices.add(4);
+        choices.add(1);     //Lettuce
+        choices.add(2);     //Tomato
+        choices.add(3);     //Carrot
+        choices.add(4);     //Cheese
+        choices.add(5);     //All
+        choices.add(6);     //exit
 
         return choices;
     }
